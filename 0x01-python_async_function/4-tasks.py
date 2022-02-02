e5ddfca47c8a568223dc3bfd21034b83667e8ac9
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2022-02-01 21:45:44 -0500

    Tasks 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,14 +6,15 @@ Description: Take the code from wait_n and alter it into a new function
 Arguments: n: int, max_delay: int = 10
 '''
 
-import random
 import asyncio
 from typing import List
+
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous Coroutine Funtion"""
-    l: List[float] = [await task_wait_random(max_delay) for i in range(n)]
-    return sorted(l)
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
+
