b1f1195981ddf642ee006ed1ed588a92fe750f7d
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2022-03-06 16:46:24 -0500

    Some changes

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,78 +1,84 @@
 #!/usr/bin/env python3
 """
-Module for test_utils.py
-0x09. Unittests and Integration Tests
-Holberton Web Stack programming Spec ― Back-end
+Tests for utils
 """
-import unittest
+
+from unittest import TestCase, mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json, memoize
-from unittest.mock import patch, Mock
+from utils import requests, get_json, access_nested_map, memoize
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """ Defines the TestAccessNestedMap Class """
+class TestAccessNestedMap(TestCase):
+    """ Test cases for utils.test_access_nested_map """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, values, path, expected):
         """
-        Test that utils.access_nested_map returns the expected result
+        Testing return
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        r = access_nested_map(values, path)
+        self.assertEqual(r, expected)
 
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(self, values, path, expected):
         """
-        Test that an exception is raised when `callable` is called with
-        any positional or keyword arguments that are also passed to
-        `assertRaises()`.
-        The test passes if exception is raised, is an error if another
-        exception is raised, or fails if no exception is raised.
+        Testing errors
         """
-        with self.assertRaises(KeyError) as exception_context_manager:
-            access_nested_map(nested_map, path)
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(values, path)
+            self.assertEqual(expected, str(e.exception))
 
 
-class TestGetJson(unittest.TestCase):
-    """ Defines the TestGetJson Class """
+class TestGetJson(TestCase):
+    """
+    Test class for utils.get_json
+    """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
         """
-        Test that utils.get_json returns the expected result
+        Test utils.get_json
         """
-        response = Mock()
-        response.json.return_value = test_payload
-        with patch('requests.get', return_value=response) as mock_get:
-            self.assertEqual(get_json(test_url), test_payload)
-        mock_get.assert_called_once_with(test_url)
+
+        mock_obj = mock.Mock()
+        mock_obj.json.return_value = test_payload
+
+        with mock.patch('requests.get', return_value=mock_obj) as n:
+            r = get_json(test_url)
+            mock_obj.json.assert_called_once()
+            self.assertEqual(r, test_payload)
 
 
-class TestMemoize(unittest.TestCase):
-    """ Defines the TestMemoize Class """
+class TestMemoize(TestCase):
+    """
+    Test class for utils.memoize
+    """
+
     def test_memoize(self):
-        """ Tests that `memoize` method works as expected """
+        """
+        Test utils.memoize
+        """
         class TestClass:
-            """ Defines the TestClass Class """
+
             def a_method(self):
-                """ Method that returns a numbers """
                 return 42
 
             @memoize
             def a_property(self):
-                """ Method that returns the `a_method` func """
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method') as mock_method:
-            test = TestClass()
-            test.a_property()
-            test.a_property()
-            mock_method.assert_called_once()
+        with mock.patch.object(TestClass, 'a_method') as mock_obj:
+            """ test a_method """
+            t_c1 = TestClass()
+            t_c1.a_property()
+            t_c1.a_property()
+            mock_obj.assert_called_once()
